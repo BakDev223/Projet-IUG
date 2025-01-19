1fe473348f77ce2316c253ef867013ea4a4201ab
Bakara <tourebakara8@gmail.com>
2025-01-19 23:41:17 +0000

    Production V-1

--- main.js
+++ main.js
@@ -4,12 +4,12 @@ const closeIcon = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" w
 const menuIcon = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" color="#000000" fill="none"><path d="M4 5L16 5" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" /><path d="M4 12L20 12" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" /><path d="M4 19L12 19" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" /></svg>`;
 
 menu.addEventListener("click", () => {
-  if (sideBar.style.left == "-200%") {
-    sideBar.style.left = 0;
-    menu.innerHTML = closeIcon;
-  } else {
-    sideBar.style.left = "-200%";
+  if (sideBar.style.left === "0px") {
+    sideBar.style.left = "-100%";
     menu.innerHTML = menuIcon;
+  } else {
+    sideBar.style.left = "0px";
+    menu.innerHTML = closeIcon;
   }
 });
 
